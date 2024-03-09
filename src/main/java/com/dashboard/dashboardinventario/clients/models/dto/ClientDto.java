bed384cb2e9ca25a36f174de5b38be2bb67830c2
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-03-09 17:56:46 +0100

    ClientDto para validar campos y limpieza de datos

---
+++ src/main/java/com/dashboard/dashboardinventario/clients/models/dto/ClientDto.java
@@ -0,0 +1,45 @@
+package com.dashboard.dashboardinventario.clients.models.dto;
+
+import jakarta.persistence.PrePersist;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ClientDto {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String fullname;
+
+    @Email(message = "Por favor,ingrese un email válido")
+    @NotBlank(message = "El email es obligatorio")
+    private String email;
+
+    @NotBlank(message = "El password es obligatorio")
+    private String password;
+
+    @NotBlank(message = "Por favor, proporcione un número de telefono válido")
+    private String phone;
+
+    @NotBlank(message = "Por favor, proporcione su dirección completa")
+    private String address;
+
+    @NotBlank(message = "El código postal es obligatorio")
+    private String postalcode;
+
+    @NotBlank(message = "Por favor, especifique la ciudad")
+    private String city;
+
+    @NotBlank(message = "Por favor, especifique el país")
+    private String country;
+
+    @PrePersist
+    public void inputClean() {
+        this.fullname = this.fullname.trim().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        this.email = this.email.trim().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        this.address = this.address.trim().toLowerCase();
+        this.postalcode = this.postalcode.trim().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        this.city = this.city.trim().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        this.country = this.country.trim().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    }
+}
