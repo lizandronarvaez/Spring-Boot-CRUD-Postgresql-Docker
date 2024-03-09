9fbfe15dc52fff8d6a18942a2f95b09c42ef558a
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-03-09 18:02:35 +0100

    Clase personalizada para enviar errores al frontend o al cliente

---
+++ src/main/java/com/dashboard/dashboardinventario/clients/validation/AuthErrorResponse.java
@@ -0,0 +1,15 @@
+package com.dashboard.dashboardinventario.clients.validation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthErrorResponse {
+    private Integer status;
+    private Object message;
+}
