6a590a6d7f1035c9eaa5762953753ae0b2062dbd
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-03-26 22:23:19 +0100

    Feat: Agregando endpoint login para los clientes

---
+++ src/main/java/com/dashboard/dashboardinventario/app/clients/controllers/ClientController.java
@@ -0,0 +1,78 @@
+package com.dashboard.dashboardinventario.app.clients.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.dashboard.dashboardinventario.app.clients.models.dto.ClientDto;
+import com.dashboard.dashboardinventario.app.clients.services.IClientService;
+
+import jakarta.validation.Valid;
+
+@CrossOrigin(origins = { "http://localhost:5173", "http://localhost:5174/" })
+@RestController
+@RequestMapping("/clients")
+@RequiredArgsConstructor
+public class ClientController {
+
+    private final IClientService clientService;
+
+    // Crear un cliente
+    @PostMapping()
+    public ResponseEntity<?> create(@Valid @RequestBody ClientDto clientDto) {
+        try {
+            return new ResponseEntity<>(clientService.saveClient(clientDto), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Mostrar todos los clientes
+    @GetMapping()
+    public ResponseEntity<?> findAll() {
+        try {
+            return new ResponseEntity<>(clientService.findAllClients(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Mostrar un cliente
+    @GetMapping("/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        try {
+            return new ResponseEntity<>(clientService.findClientById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody ClientDto clientDto, @PathVariable Integer id) {
+        try {
+            return new ResponseEntity<>(clientService.updateClient(clientDto, id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        try {
+            return new ResponseEntity<>(clientService.deleteClient(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Login cliente
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody ClientDto clientDto) {
+        try {
+            return new ResponseEntity<>(clientService.loginClient(clientDto), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
