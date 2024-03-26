735df0a1f11f8d3c73c82e6ad3aa272da6f7a5b4
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-03-26 22:29:10 +0100

    Feat: Logica de productos

---
+++ src/main/java/com/dashboard/dashboardinventario/app/products/services/IProductService.java
@@ -0,0 +1,22 @@
+package com.dashboard.dashboardinventario.app.products.services;
+
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.dashboard.dashboardinventario.app.products.controllers.AuthResponse;
+import com.dashboard.dashboardinventario.app.products.models.dto.ProductDto;
+import com.dashboard.dashboardinventario.app.products.models.entity.ProductEntity;
+
+
+public interface IProductService {
+
+    AuthResponse saveProduct(MultipartFile file, ProductDto productDto);
+
+    List<ProductEntity> findAllProduct();
+
+    AuthResponse findProductById(Integer id);
+
+    AuthResponse updateProduct(MultipartFile file, ProductDto productDto, Integer id);
+
+    AuthResponse deleteProduct(Integer id);
+}
