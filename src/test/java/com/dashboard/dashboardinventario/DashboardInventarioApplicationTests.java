578c7028c4282b3760fbbc3138b7bba9a85e86d0
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-04-29 21:40:26 +0200

    commit

---
+++ src/test/java/com/dashboard/dashboardinventario/DashboardInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.dashboardinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
